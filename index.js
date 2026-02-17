24905cd84af5fa6f6f7404823e73ad48856df824
TorresJim <torresguarino17@gmail.com>
2026-02-17 23:41:55 +0800

    adding javascript

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import { menuArray } from '/data.js'
 
+const {name, ingredients, id, price, emoji} = menu
+
+function getMenuItems(){
+    let menuItems = ''
+
+    menu.forEach(function(menu){
+        menuItems += `
+        <div class="items">
+            <div class="items-inner">
+                <img src="${menu.emoji}" class="item-emoji">
+                <div>
+                    <p class="item-name">${menu.name}</p>
+                    <p class="item-ingredients">${menu.ingredients}</p>
+                    <p class="item-price">$${menu.price}</p>
+                </div>            
+            </div>
+        </div>
+        `
+    })
+    return menuItems
+}
+
+function render(){
+    document.getElementById('menu').innerHTML = getMenuItems()
+}
+render()
+
+console.log("i love my mom")
