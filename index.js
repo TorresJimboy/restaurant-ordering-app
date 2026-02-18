f3b0993bd98bfd8cd64029277184031016f4be0e
TorresJim <torresguarino17@gmail.com>
2026-02-18 18:02:38 +0800

    javascript updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { menuArray } from './data.js'
 
+// Render Items
 function getMenuItems(){
     let menuItems = ''
 
@@ -13,7 +14,7 @@ function getMenuItems(){
                     <p class="item-ingredients">${menu.ingredients}</p>
                     <p class="item-price">$${menu.price}</p>
                 </div>
-                <button class="item-add">+</button>           
+                <button class="item-add" id="item-add" data-add="${menu.id}">+</button>           
             </div>
         </div>
         `
@@ -26,3 +27,89 @@ function render(){
 }
 
 render()
+
+// Clicks Handler
+
+document.addEventListener('click', function(e){
+    if (e.target.dataset.add){
+        handleAddClick(e.target.dataset.add)
+    }
+    else if (e.target.dataset.remove){
+        handleRemoveClick(e.target.dataset.remove)
+    }
+    
+})
+
+// Add Items
+let ordersArray = []
+function handleAddClick(itemId){
+    cart.hidden = false
+
+    const targetItemObj = menuArray.find(item =>
+        item.id === Number(itemId)
+    )
+    
+    const existingItem = ordersArray.find(item =>
+        item.id === targetItemObj.id
+    )
+
+    if (existingItem){
+        existingItem.quantity += 1
+    } else {
+        ordersArray.push({
+            ...targetItemObj, 
+            quantity: 1
+        })
+    }
+    
+    renderOrders()
+    renderTotal()
+}
+
+// Render Orders
+
+function renderOrders(){
+
+    let orderItem = ''
+
+    ordersArray.forEach(function(item){
+        orderItem += `
+            <div class="order-items">
+                <p class="item-count" id="item-count">x${item.quantity}</p>
+                <p class="order-name">${item.name}</p>
+                <button class="order-remove-btn" data-remove="${item.id}">remove</button>
+                <p class="order-price" id="order-price">$${item.price * item.quantity}</p>
+            </div>
+        `
+    })
+
+    document.getElementById('orders-container').innerHTML = orderItem
+}
+
+// Remove Items
+
+function handleRemoveClick(itemId){
+    const targetItemObj = menuArray.find(item =>
+        item.id === Number(itemId)
+    )
+    ordersArray.pop(targetItemObj)
+    renderOrders()
+}
+
+// Render Total Amount
+
+function renderTotal(){
+    const total = ordersArray.reduce((sum, item) => sum + item.price * item.quantity, 0)
+    
+    document.getElementById("total-amount").textContent = `$${total}`
+}
+
+
+
+
+
+
+
+
+
+
